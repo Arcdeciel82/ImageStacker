ba94ea42097886524392d3b3e4fab594074dbaaf
curjlync <curjlync@ut.utm.edu>
2022-09-22 11:59:20 -0500

    ui added

--- main.cpp
+++ main.cpp
@@ -1,30 +1,37 @@
 #include <iostream>
+#include <iomanip>
 #include <string>
+#include <sstream>
 
 #include "stacker.h"
 
 using namespace std;
 
 int main() {
-  string sInputPaths[] = {
-    "ppms/cone_nebula/cone_nebula_001.ppm", 
-    "ppms/cone_nebula/cone_nebula_002.ppm", 
-    "ppms/cone_nebula/cone_nebula_003.ppm", 
-    "ppms/cone_nebula/cone_nebula_004.ppm", 
-    "ppms/cone_nebula/cone_nebula_005.ppm", 
-    "ppms/cone_nebula/cone_nebula_006.ppm", 
-    "ppms/cone_nebula/cone_nebula_007.ppm", 
-    "ppms/cone_nebula/cone_nebula_008.ppm", 
-    "ppms/cone_nebula/cone_nebula_009.ppm", 
-    "ppms/cone_nebula/cone_nebula_010.ppm", 
-  };
-  
+  string sInputBaseFilePath = "ppms/";
   stacker s1;
-  for (auto& path : sInputPaths) {
-    s1.addImage(path);
+  string sImage;
+  string sOutputFile;
+  int num;
+  cout << "Please enter the image you wish to stack: \n";
+  getline(cin, sImage);
+  cout << "Please enter the number of images: \n";
+  cin >> num;
+  cout << "Stacking images:\n";
+  
+  for (int i = 1; i <= num; i++) {
+    stringstream ssFilePath;
+    ssFilePath << sInputBaseFilePath << sImage << "/" << sImage;
+    ssFilePath << "_" << setfill('0') << setw(3) << right << i << ".ppm";
+    cout << "\t" << ssFilePath.str() << endl;
+    s1.addImage(ssFilePath.str());
   }
+  cout << endl;
   s1.stack();
-  s1.writeToFile("test.ppm");
+  sOutputFile = sImage + ".ppm";
+  s1.writeToFile(sOutputFile);
+  cout << "Stacking completed.\n";
+  cout << "Output written to: " << sOutputFile << endl; 
 
   return 0;
 }
